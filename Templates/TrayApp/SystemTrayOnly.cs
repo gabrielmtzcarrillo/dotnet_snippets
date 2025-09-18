4072c90554fd9ff6594e1486aa80c8186b67f204
Gabriel Mtz <gabrielmtzcarrillo@users.noreply.github.com>
2025-09-18 15:21:50 -0600

    Add Windows Forms tray app template
    Introduces a new TrayApp template project for Windows Forms applications with a system tray icon, context menu, and exit functionality. Includes project files, solution, icon resource, and supporting code for tray-only operation.

---
+++ Templates/TrayApp/SystemTrayOnly.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrayApp
+{
+    internal class SystemTrayOnly: ApplicationContext
+    {
+        NotifyIcon _icon;
+        ContextMenuStrip _menu;
+
+        public SystemTrayOnly()
+        {
+            _menu = new ContextMenuStrip();
+            _menu.Items.Add("Exit", null, OnExit);
+
+            _icon = new NotifyIcon();
+            _icon.Icon = Icons.ResourceManager.GetObject("AppIcon") as System.Drawing.Icon;
+            _icon.ContextMenuStrip = _menu;
+
+            _icon.Visible = true;
+        }
+
+        void OnExit(object? sender, EventArgs e)
+        {
+            _icon.Visible = false;
+            Application.Exit();
+        }
+    }
+}
